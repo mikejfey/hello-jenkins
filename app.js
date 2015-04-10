d705b2c59dc5377d6b69a61d06fad6bb0681f7d5
Your Name <michael.j.fey@hp.com>
2015-04-10 08:23:54 -0600

    changed text 5

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello cruzman fey');
+  res.send('hello cruzman fey stachia');
 });
  
 app.listen(process.env.PORT || 5000);
